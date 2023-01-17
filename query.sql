e87d801ff81f06946715328135d4b2e02e8a0b4d
dhinakaran2k <112272405+dhinakaran2k@users.noreply.github.com>
2023-01-17 12:37:13 +0530

    Add files via upload

---
+++ query.sql
@@ -0,0 +1,9 @@
+SELECT * FROM HospitalManagementSystem.superAdmin;
+
+SELECT * FROM HospitalManagementSystem.admin;
+
+SELECT * FROM HospitalManagementSystem.patient;
+
+SELECT * FROM HospitalManagementSystem.doctors;
+
+SELECT * FROM HospitalManagementSystem.appoinment;
